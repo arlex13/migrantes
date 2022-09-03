057151a9c79b3b9817e48c3ce26a5625770e40fc
davidbgemin7357 <davidbgemin.dev@gmail.com>
2022-09-03 14:15:04 -0500

    color

--- frontend/src/pages/publico/index.jsx
+++ frontend/src/pages/publico/index.jsx
@@ -17,7 +17,7 @@ const PaginaPublica = () => {
 
     return (
         <>
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded " style={{backgroundColor: "#17527f"}}>
     <div className="container flex flex-wrap justify-between items-center mx-auto">
       <div className="flex items-center">
           <div className="mr-3 h-6 sm:h-9"></div>
