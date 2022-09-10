4dad2667e6a19a3d3bb1e66787850d9af55c2b87
arlex13 <arlexcaal@gmail.com>
2022-09-09 22:35:06 -0600

    fixed

--- api/viewsets/estadisticas.py
+++ api/viewsets/estadisticas.py
@@ -60,21 +60,24 @@ class EstadisticasViewSet(viewsets.ViewSet):
 
     query = Migrante.objects.filter(created__year=anio).values('id', 'genero','created')
 
+    hombres = { "label":"Hombres" ,"backgroundColor" : "rgb(75, 192, 192)", 'data': map(lambda x:0, MESES)}
+    mujeres = { "label":"Mujeres" ,"backgroundColor" : "rgb(53, 162, 250)", 'data': map(lambda x:0, MESES)}
+    lgbti = { "label":"LGBTI" ,"backgroundColor" : "rgb(155, 99, 132)", 'data': map(lambda x:0, MESES)}
+
     tabla = pd.DataFrame(query)
-    tabla['conteo'] = 1
-    tabla['mes'] = pd.to_datetime(tabla['created']).dt.month
-    tabla.sort_values(by="mes", inplace=True)
-    tabla_hombres = tabla.query("genero=='Hombre'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_mujer = tabla.query("genero=='Mujer'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_lgbti = tabla.query("genero=='LGBTI'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
 
-    hombres = { "label":"Hombres" ,"backgroundColor" : "rgb(75, 192, 192)"}
-    mujeres = { "label":"Mujeres" ,"backgroundColor" : "rgb(53, 162, 250)"}
-    lgbti = { "label":"LGBTI" ,"backgroundColor" : "rgb(155, 99, 132)"}
+    if tabla.shape[0] > 1:
+      tabla['conteo'] = 1
+      tabla['mes'] = pd.to_datetime(tabla['created']).dt.month
+      tabla.sort_values(by="mes", inplace=True)
+      tabla_hombres = tabla.query("genero=='Hombre'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_mujer = tabla.query("genero=='Mujer'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_lgbti = tabla.query("genero=='LGBTI'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
 
-    hombres['data'] = self.__obtener_dato_mes(tabla_hombres.to_dict("records"))
-    mujeres['data'] = self.__obtener_dato_mes(tabla_mujer.to_dict("records"))
-    lgbti['data'] = self.__obtener_dato_mes(tabla_lgbti.to_dict("records"))
+
+      hombres['data'] = self.__obtener_dato_mes(tabla_hombres.to_dict("records"))
+      mujeres['data'] = self.__obtener_dato_mes(tabla_mujer.to_dict("records"))
+      lgbti['data'] = self.__obtener_dato_mes(tabla_lgbti.to_dict("records"))
 
     data_genero =[hombres, mujeres, lgbti]
     return data_genero
@@ -83,27 +86,27 @@ class EstadisticasViewSet(viewsets.ViewSet):
     anio= self.request.query_params.get('anio', date.today().year)
 
     query = Migrante.objects.filter(created__year=anio).values('id', 'estado_civil','created')
+    
+    soltero = { "label":"Soltero" ,"backgroundColor" : "rgb(75, 192, 192)",'data': map(lambda x:0, MESES)}
+    casado = { "label":"Casado" ,"backgroundColor" : "rgb(53, 162, 250)",'data': map(lambda x:0, MESES)}
+    union_libre = { "label":"Union Libre" ,"backgroundColor" : "rgb(155, 99, 132)",'data': map(lambda x:0, MESES)}
+    d_s_v = { "label":"D/S/V" ,"backgroundColor" : "rgb(100, 102, 100)",'data': map(lambda x:0, MESES)}
 
     tabla = pd.DataFrame(query)
-    tabla['conteo'] = 1
-    tabla['mes'] = pd.to_datetime(tabla['created']).dt.month
-    tabla.sort_values(by="mes", inplace=True)
+    if tabla.shape[0] > 1:
+      tabla['conteo'] = 1
+      tabla['mes'] = pd.to_datetime(tabla['created']).dt.month
+      tabla.sort_values(by="mes", inplace=True)
 
+      tabla_soltero = tabla.query("estado_civil=='Soltero'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_casado = tabla.query("estado_civil=='Casado'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_union_libre = tabla.query("estado_civil=='Union Libre'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_d_s_v = tabla.query("estado_civil=='D/S/V'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
 
-    tabla_soltero = tabla.query("estado_civil=='Soltero'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_casado = tabla.query("estado_civil=='Casado'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_union_libre = tabla.query("estado_civil=='Union Libre'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_d_s_v = tabla.query("estado_civil=='D/S/V'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-
-    soltero = { "label":"Soltero" ,"backgroundColor" : "rgb(75, 192, 192)"}
-    casado = { "label":"Casado" ,"backgroundColor" : "rgb(53, 162, 250)"}
-    union_libre = { "label":"Union Libre" ,"backgroundColor" : "rgb(155, 99, 132)"}
-    d_s_v = { "label":"D/S/V" ,"backgroundColor" : "rgb(100, 102, 100)"}
-
-    soltero['data'] = self.__obtener_dato_mes(tabla_soltero.to_dict("records"))
-    casado['data'] = self.__obtener_dato_mes(tabla_casado.to_dict("records"))
-    union_libre['data'] = self.__obtener_dato_mes(tabla_union_libre.to_dict("records"))
-    d_s_v['data'] = self.__obtener_dato_mes(tabla_d_s_v.to_dict("records"))
+      soltero['data'] = self.__obtener_dato_mes(tabla_soltero.to_dict("records"))
+      casado['data'] = self.__obtener_dato_mes(tabla_casado.to_dict("records"))
+      union_libre['data'] = self.__obtener_dato_mes(tabla_union_libre.to_dict("records"))
+      d_s_v['data'] = self.__obtener_dato_mes(tabla_d_s_v.to_dict("records"))
 
     data_estado_civil =[soltero, casado, union_libre, d_s_v]
     return data_estado_civil
@@ -114,25 +117,26 @@ class EstadisticasViewSet(viewsets.ViewSet):
 
     query = Migrante.objects.filter(created__year=anio).values('id', 'escolaridad','created')
 
+    primaria = { "label":"Primaria" ,"backgroundColor" : "rgb(75, 192, 192)",'data': map(lambda x:0, MESES)}
+    secundaria = { "label":"Secundaria" ,"backgroundColor" : "rgb(53, 162, 250)",'data': map(lambda x:0, MESES)}
+    superior = { "label":"Superior" ,"backgroundColor" : "rgb(155, 99, 132)",'data': map(lambda x:0, MESES)}
+    analfabeta = { "label":"Analfabeta" ,"backgroundColor" : "rgb(100, 102, 100)",'data': map(lambda x:0, MESES)}
+
     tabla = pd.DataFrame(query)
-    tabla['conteo'] = 1
-    tabla['mes'] = pd.to_datetime(tabla['created']).dt.month
-    tabla.sort_values(by="mes", inplace=True)
+    if tabla.shape[0] > 1:	
+      tabla['conteo'] = 1
+      tabla['mes'] = pd.to_datetime(tabla['created']).dt.month
+      tabla.sort_values(by="mes", inplace=True)
 
-    tabla_primaria = tabla.query("escolaridad=='Primaria'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_secundaria = tabla.query("escolaridad=='Secundaria'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_superior = tabla.query("escolaridad=='Superior'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
-    tabla_analfabeta = tabla.query("escolaridad=='Analfabeta'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_primaria = tabla.query("escolaridad=='Primaria'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_secundaria = tabla.query("escolaridad=='Secundaria'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_superior = tabla.query("escolaridad=='Superior'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
+      tabla_analfabeta = tabla.query("escolaridad=='Analfabeta'").groupby(by=['mes'], as_index=False).agg({"conteo":"sum"})
 
-    primaria = { "label":"Primaria" ,"backgroundColor" : "rgb(75, 192, 192)"}
-    secundaria = { "label":"Secundaria" ,"backgroundColor" : "rgb(53, 162, 250)"}
-    superior = { "label":"Superior" ,"backgroundColor" : "rgb(155, 99, 132)"}
-    analfabeta = { "label":"Analfabeta" ,"backgroundColor" : "rgb(100, 102, 100)"}
-
-    primaria['data'] = self.__obtener_dato_mes(tabla_primaria.to_dict("records"))
-    secundaria['data'] = self.__obtener_dato_mes(tabla_secundaria.to_dict("records"))
-    superior['data'] = self.__obtener_dato_mes(tabla_superior.to_dict("records"))
-    analfabeta['data'] = self.__obtener_dato_mes(tabla_analfabeta.to_dict("records"))
+      primaria['data'] = self.__obtener_dato_mes(tabla_primaria.to_dict("records"))
+      secundaria['data'] = self.__obtener_dato_mes(tabla_secundaria.to_dict("records"))
+      superior['data'] = self.__obtener_dato_mes(tabla_superior.to_dict("records"))
+      analfabeta['data'] = self.__obtener_dato_mes(tabla_analfabeta.to_dict("records"))
 
     data_escolaridad =[primaria, secundaria, superior, analfabeta]
     return data_escolaridad
